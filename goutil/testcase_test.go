5f49e711fd009d9a2c3819c8533690402ff1836b
yy.xie <yanyang.xie@gmail.com>
2022-10-24 12:20:51 +0800

    remove go dependence

--- goutil/testcase_test.go
+++ goutil/testcase_test.go
@@ -1,7 +1,6 @@
 package goutil
 
 import (
-	"github.comcast.com/viper-cog/goutil/assert"
 	"testing"
 )
 
@@ -22,27 +21,9 @@ func TestGetTestCaseShortName(t *testing.T) {
 	}
 
 	for _, tc := range testCases {
-		assert.Equals(t, tc.want, GetTestCaseShortName(tc.caseName))
+		if tc.want != GetTestCaseShortName(tc.caseName) {
+			t.Fatalf("fail to validate test case short name. want: %v, got: %v", tc.want, GetTestCaseShortName(tc.caseName))
+		}
 	}
 }
 
-func TestIsCaseInList(t *testing.T) {
-	type testCase struct {
-		caseName string
-		caseList string
-		want     bool
-	}
-
-	testCases := []testCase{
-		// contain "RIO_"
-		{"TestArchivedAfterConfiguredTimeRIO_RCCS_AA_TC1", "RCCS_AA_TC1,RCCS_AA_TC2", true},
-		// not contain "RIO_"
-		{"TestArchivedAfterConfiguredTime_RCCS_AA_TC1", "RCCS_AA_TC1,RCCS_AA_TC2", false},
-		// contain multiple "RIO_"
-		{"TestArchivedAfterConfiguredTime_RIO_RCCS_AA_TC1_RIO_AA_Test2", "RCCS_AA_TC1,RCCS_AA_TC2,AA_Test2,RCCS_AA_TC3,RCCS_AA_TC5", true},
-	}
-
-	for _, tc := range testCases {
-		assert.Equals(t, tc.want, IsCaseInList(tc.caseName, tc.caseList))
-	}
-}
